69170fd6bb587fc35b74fb6534dabb4c9deaf667
Julien Froidefond <julien.froidefond@cdbdx.biz>
2026-02-07 17:55:07 +0100

    feat(1-5): Add custom display font and update global styles; enhance layout with new font integration

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -3,7 +3,7 @@ import { Footer } from "../components/Footer";
 
 export default function FrontLayout(props: LayoutProps<"/">) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="font-display flex min-h-screen flex-col">
       <Nav />
       <main className="flex-1">{props.children}</main>
       <Footer />
