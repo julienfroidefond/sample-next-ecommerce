f8879e22648a5a9a546d96df95328c88d5024580
Julien Froidefond <julien.froidefond@cdbdx.biz>
2026-02-07 17:55:07 +0100

    feat(1-5): Add custom display font and update global styles; enhance layout with new font integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +11,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const fontDisplay = localFont({
+  src: "./fonts/Dancing_Script/DancingScript-VariableFont_wght.ttf",
+  variable: "--font-display",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${fontDisplay.variable} min-h-screen antialiased`}
       >
         {children}
       </body>
