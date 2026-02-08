0ea21d24d350e3f633dc82387f1d30c7bc351f26
Julien Froidefond <julien.froidefond@cdbdx.biz>
2026-02-08 16:00:45 +0100

    refactor(layout): Remove unnecessary font class from layout component for cleaner styling

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -6,7 +6,7 @@ import { Nav } from "../components/Nav";
 export default function FrontLayout(props: LayoutProps<"/">) {
   return (
     <CartProviderWrapper>
-      <div className="font-display flex min-h-screen flex-col">
+      <div className="flex min-h-screen flex-col">
         <Nav cartSummary={<CartSummary />} />
         <main className="flex-1">{props.children}</main>
         <Footer />
